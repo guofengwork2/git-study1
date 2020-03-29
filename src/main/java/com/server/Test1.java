422b4ea88ad79213f200105b146f2a4681e6a095
guofengwork2 <guofengwork2@163.com>
2020-03-29 20:35:00 +0800

    my first commit in eclipse

---
+++ src/main/java/com/server/Test1.java
@@ -0,0 +1,9 @@
+package com.server;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		System.out.println("lalala");
+	}
+	
+}
---
+++ src/test/java/com/server/Test1.java
@@ -0,0 +1,10 @@
+package com.server;
+
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		System.out.println("lalala");
+	}
+	
+}

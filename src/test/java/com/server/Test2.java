7c57ec83c71d415e770a2fff35bed7c90d9bcd4b
guofengwork2 <guofengwork2@163.com>
2020-03-29 22:10:34 +0800

    修改了

--- src/test/java/com/server/Test2.java
+++ src/test/java/com/server/Test2.java
@@ -1,11 +1,10 @@
 package com.server;
 
+public class Test2 {
 
-public class Test1 {
-	
 	public static void main(String[] args) {
 		System.out.println("lalala");
 		System.out.println("Test");
 	}
-	
+
 }
